861af9c41766ee875acf3c365683bf003741f6e8
guybnd <51272552+guybnd@users.noreply.github.com>
2026-05-14 00:16:25 +1000

    Update release.ts

--- engine/src/release.ts
+++ engine/src/release.ts
@@ -1,7 +1,11 @@
 import fs from 'fs/promises';
 import { existsSync } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import matter from 'gray-matter';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function run() {
   const args = process.argv.slice(2);
